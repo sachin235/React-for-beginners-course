e18e782ac6be1f4aa60ceb98f0379861422aa3e0
Anant Rungta <anantrungta1999@gmail.com>
2019-06-26 22:46:56 +0530

    Updates

---
+++ precache-manifest.57264f76edf01bfc5a9c7e03b9fdc6e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c207f1313d94d5f047d6c2d54d8234dd",
+    "url": "/anu/index.html"
+  },
+  {
+    "revision": "c66a6f38648663d19131",
+    "url": "/anu/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1043c995eac91d7833cd",
+    "url": "/anu/static/js/2.f22f7976.chunk.js"
+  },
+  {
+    "revision": "c66a6f38648663d19131",
+    "url": "/anu/static/js/main.417118ed.chunk.js"
+  },
+  {
+    "revision": "8d9f41af66c4bd90b90f",
+    "url": "/anu/static/js/runtime~main.bdbc5a26.js"
+  }
+]);
